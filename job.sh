3b8ae530a613ce211f378dde6f5ee9c3dd998432
xuyongzhi <buaaxyz@yeah.net>
2018-07-10 21:48:15 +1000

    add job.sh

---
+++ job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -q gpu
+#PBS -l walltime=20:00:00
+#PBS -l mem=16GB
+#PBS -l jobfs=0GB
+#PBS -l ngpus=2
+#PBS -l ncpus=6
+## For licensed software, you have to specify it to get the job running. For unlicensed software, you should also specify it to help us analyse the software usage on our system.
+#PBS -l software=tensorflow/1.8-cudnn7.1-python2.7
+
+## The job will be executed from current working directory instead of home.
+#PBS -l wd 
+#PBS -r y
+##PBS -M y.xu@unsw.edu.au
+##PBS -m abe
+
+module load  tensorflow/1.8-cudnn7.1-python2.7
+module list
+ 
+
+#------------------------------------------------------------------------
+python train_multi_gpu.py --num_gpus 2
--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #CUDA_VISIBLE_DEVICES=0,1 python train_multi_gpu.py --num_gpus 2 --normal
 
-CUDA_VISIBLE_DEVICES=0,1 python train_multi_gpu.py --num_gpus 2
+python train_multi_gpu.py --num_gpus 2
